3911d4f59892fe9466ec82ab9bd6028ca3a127d5
Istiaq Nirab <istiaqanirab202@gmail.com>
2020-12-10 22:12:48 +0600

    remove post type from nav menu

--- includes/Illuminate/Subscriptions.php
+++ includes/Illuminate/Subscriptions.php
@@ -49,7 +49,7 @@ class Subscriptions
             "rest_controller_class" => "WP_REST_Posts_Controller",
             "has_archive" => false,
             "show_in_menu" => false,
-            "show_in_nav_menus" => true,
+            "show_in_nav_menus" => false,
             "exclude_from_search" => false,
             "capability_type" => "post",
             "map_meta_cap" => true,
