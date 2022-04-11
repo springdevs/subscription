4460d8bbda129d510d31ed0671f8ccddc4322278
Istiaq Nirab <40653908+Ok9xNirab@users.noreply.github.com>
2022-04-12 04:00:55 +0600

    update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,3 +1,4 @@
+composer install --no-dev
 rm -r composer.json
 rm -r composer.lock
 
@@ -6,6 +7,12 @@ rm -r README.md
 rm -rf .git/
 rm -r .editorconfig
 rm -r .gitignore
+
+rm -rf bin/
+rm -rf tests/
+rm -r .phpcs.xml
+rm -r .travis.yml
+rm -r phpunit.xml
 
 echo "Production Ready 📦"
 rm -r clean.sh
